8ddf52f9c04caba4dbfdd8b69660a02f01403e1d
Merry-robot <elijahjubrey@gmail.com>
2025-10-13 17:05:39 -0700

    Change scouting app name and add image plaeholder

--- frontend/src/components/pages/projects.jsx
+++ frontend/src/components/pages/projects.jsx
@@ -1,13 +1,17 @@
 import '../css/global.css';
-import '../css/projects.css'
+import '../css/projects.css';
 
 export default function projects() {
   return (
     <div>
       <div id="project-column">
         <h1 class="">Robots</h1>
-        <div id="robot-block"></div>
-        <h1>PSS</h1>  // "Paragon Scouting Solution" my working name for the scouting app 
+        <div id="robots-block">
+          <div id="2025-card">
+            <image src={robot25} alt="2025 Robot" />
+          </div>
+        </div>
+        <h1>SuperScout</h1>  
       </div>
     </div>
   );
