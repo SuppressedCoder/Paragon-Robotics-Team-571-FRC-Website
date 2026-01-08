1fc9c44fbcb6d4aae278d5dc6807b781a9162f7d
Elijah Jubrey <elijahjubrey@gmail.com>
2026-01-08 08:17:40 -0500

    add header footer and design change

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<script>
+
+    import logo from "$lib/assets/widelogo.png";
+
+</script>
+
+<div class="bg-black p-4">
+    <img src={logo} alt="logo" class="w-1/3 h-auto rounded-3xl bg-zinc-800">
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Navbar from "$lib/components/Navbar.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+</script>
+
+<div class="justify-between flex flex-col h-screen">
+    <Navbar />
+    <Footer />
+</div>
